ea236f2d36a9332657729215df4876f69f7afefc
Mehmet CESUR <mehmetceesur@gmail.com>
2024-08-09 20:29:26 +0300

    Todo Updated added

--- to-do-app/src/components/Todo.tsx
+++ to-do-app/src/components/Todo.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { FaCheck } from "react-icons/fa";
 import { FaRegEdit } from "react-icons/fa";
 import { TodoType } from '../types/Types';
 import { useDispatch } from 'react-redux';
-import { removeTodoById } from '../redux/todoSlice';
+import { removeTodoById, updateTodo } from '../redux/todoSlice';
+import { DiVim } from 'react-icons/di';
 
 interface TodoProps{
 
@@ -18,20 +19,36 @@ function Todo({todoProps}:TodoProps) {
 
   const dispatch =useDispatch();
 
+  const [editable,setEditable]= useState<boolean>(false);
+
+  const [newTodo,setNewTodo]=useState<string>(content);
+
   const handleRemoveTodo= () =>{
       dispatch(removeTodoById(id))
   }
   
-
+  const handleUpdateTodo = ()=> {
+      const payload :TodoType = {
+        id: id,
+        content :newTodo
+      }
+      dispatch(updateTodo(payload))
+      setEditable(false)
+  }
+  
   return (
 
     <div style={{display:'flex', flexDirection:'row', alignItems:'center',justifyContent:'space-between', 
            border:'1px solid lightgrey',padding:'16px',marginTop:'25px',borderRadius:'5px'}} >
         
-        <div>{content}</div>
+            {editable ? <input type='text'  style={{width:'400px',border:'none',borderBottom:'1px solid lightgrey' ,outline:'none '}}
+            value={newTodo}
+             onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setNewTodo(e.target.value)}  />: <div>{content}</div>}
           <div>
              <IoMdRemoveCircleOutline onClick={handleRemoveTodo} className='icons' style={{marginRight:'8px'}} />
-             <FaRegEdit className='icons' />
+             
+              {editable ? <FaCheck className='icons' onClick={handleUpdateTodo} /> : <FaRegEdit onClick={()=>setEditable(true)} className='icons' />}
+             
         
         
           </div>
