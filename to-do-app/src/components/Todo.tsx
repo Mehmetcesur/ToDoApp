47cb79f865987c5b00091f82eed27a1a66d2dc92
Mehmet CESUR <mehmetceesur@gmail.com>
2024-08-09 19:59:14 +0300

    Todo added and Todo deleted

--- to-do-app/src/components/Todo.tsx
+++ to-do-app/src/components/Todo.tsx
@@ -2,16 +2,35 @@ import React from 'react'
 import { IoMdRemoveCircleOutline } from "react-icons/io";
 import { FaCheck } from "react-icons/fa";
 import { FaRegEdit } from "react-icons/fa";
+import { TodoType } from '../types/Types';
+import { useDispatch } from 'react-redux';
+import { removeTodoById } from '../redux/todoSlice';
 
-function Todo() {
+interface TodoProps{
+
+  todoProps : TodoType
+}
+
+
+function Todo({todoProps}:TodoProps) {
+
+  const {id,content} = todoProps;
+
+  const dispatch =useDispatch();
+
+  const handleRemoveTodo= () =>{
+      dispatch(removeTodoById(id))
+  }
+  
+
   return (
 
     <div style={{display:'flex', flexDirection:'row', alignItems:'center',justifyContent:'space-between', 
            border:'1px solid lightgrey',padding:'16px',marginTop:'25px',borderRadius:'5px'}} >
         
-        <div>Ben ilk todoyum</div>
+        <div>{content}</div>
           <div>
-             <IoMdRemoveCircleOutline className='icons' style={{marginRight:'8px'}} />
+             <IoMdRemoveCircleOutline onClick={handleRemoveTodo} className='icons' style={{marginRight:'8px'}} />
              <FaRegEdit className='icons' />
         
         
--- to-do-app/src/components/TodoList.tsx
+++ to-do-app/src/components/TodoList.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Todo from './Todo'
+import { useSelector } from 'react-redux'
+import { RootState } from '../redux/store'
+import { TodoType } from '../types/Types'
+
 
 function TodoList() {
+
+  const{todos} = useSelector((state:RootState)=>state.todo)
+
+
   return (
-    <Todo/>
+
+    <div>
+        {todos && todos.map((todo:TodoType)=>(
+      <Todo key={todo.id} todoProps = {todo} />
+    ))}
+
+    </div>
+    
   )
 }
 
